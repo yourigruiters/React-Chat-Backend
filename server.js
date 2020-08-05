6745d48ad363cc6b2ac7975fc4b3ea7aea3d5dd8
Youri Gruiters <i-youri-i@hotmail.com>
2020-08-05 16:43:21 +0200

    Fix: implemented authentication logic front-end and back-end

--- server.js
+++ server.js
@@ -6,10 +6,48 @@ const io = require("socket.io")(http);
 const cors = require("cors");
 const PORT = process.env.PORT || 5000;
 
+const users = [
+  {
+    username: "iSnaek",
+    color: "#381953"
+  },
+  {
+    username: "UsedToLoveYa",
+    color: "#939128"
+  }
+];
+const isTyping = [
+  {
+    username: "iSnaek",
+    color: "#381953"
+  },
+  {
+    username: "UsedToLoveYa",
+    color: "#939128"
+  }
+];
+
+const colors = ["#183734", "#885522"];
+
 app.use(cors());
 
 app.get("/", (req, res) => {
   res.send("Server up and running");
+});
+
+app.get("/api/user/:username", (req, res) => {
+  const username = req.params.username;
+  const userIndex = users.find(
+    (user) => user.username.toLowerCase() === username.toLowerCase()
+  );
+
+  console.log("oleee");
+
+  if (!userIndex) {
+    res.status(200).json({ message: "Username free" });
+  } else {
+    res.status(202).json({ error: "Username already taken" });
+  }
 });
 
 io.on("connection", (socket) => {
